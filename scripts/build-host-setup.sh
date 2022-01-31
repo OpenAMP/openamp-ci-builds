1438ad945d731247dcca234d42372c864bbff233
Bill Mills <bill.mills@linaro.org>
2022-01-30 20:38:40 -0500

    scripts: initial build host setup script
    Automate setting up a new build host
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

---
+++ scripts/build-host-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# setup host for building
+sudo apt-get update
+
+if [ -r ~/.aws/credentials ]; then
+    sudo apt-get install -y s3fs
+    mkdir -p ~/net/openamp-builds
+    s3fs -o profile=openamp-builds-rw openamp-builds ~/net/openamp-builds
+fi
+
+# Yocto/OE requirements
+sudo apt-get install -y gawk wget git-core diffstat unzip texinfo \
+  build-essential chrpath socat cpio python python3 python3-pip \
+  python3-pexpect xz-utils debianutils iputils-ping curl git
+
+# we need kas
+if ! which kas; then
+    pip3 install kas
+    if ! which kas; then
+        # fix up for this time, assume .bashrc will get it next time
+        PATH=~/.local/bin:$PATH
+    fi
+fi
+
