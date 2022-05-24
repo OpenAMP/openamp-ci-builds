6f1dd490deb5af201ee0693a6b36dccfe967143f
Bill Mills <bill.mills@linaro.org>
2022-05-24 14:46:42 -0400

    xilinx-vendor: update to 2022.1 release and add kv260
    Update to Xilinx's latest release.
    There are some new package dependencies listed.
    Updates for Honister (yocto 3.4) branch.
    Fix local.conf hacks to be reproducible and do them all in one spot.
    Change quoting to be easier to read.
    Disable PMU ROM fetch as it is part of layer now.
    Build for zcu102 AND kv260.
    Delete "run" step that was not a run step
    
    Original and updates based on google docs from
    tanmay Shah <tanmays@xilinx.com>
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- for-ref/xilinx-vendor.sh
+++ for-ref/xilinx-vendor.sh
@@ -14,7 +14,8 @@ sudo apt-get update
 # Xilinx xsct binary needs explictly libtinfo5.
 # Ubuntu 20.04 uses libtinfo6 when you install libtinfo-dev
 sudo apt-get install -y chrpath diffstat gawk qemu-utils curl \
-    build-essential python3-distutils libtinfo5 libidn11-dev
+    build-essential python3-distutils libtinfo5 libtinfo-dev \
+    libidn11-dev libgmp3-dev zstd
 
 # repo will work if python is python2 or python3 but it needs to be something
 # if the user already has one don't touch it
@@ -57,7 +58,7 @@ mkdir -p $XILINX_YOCTO_ROOT
 cd $XILINX_YOCTO_ROOT
 
 echo "*** getting meta data"
-$REPO init -u https://github.com/Xilinx/yocto-manifests.git -b rel-v2021.2
+$REPO init -u https://github.com/Xilinx/yocto-manifests.git -b rel-v2022.1
 $REPO sync
 source setupsdk
 
@@ -66,9 +67,21 @@ source setupsdk
 # local.conf mods
 echo "*** mod local.conf"
 CONF=$XILINX_YOCTO_ROOT/build/conf/local.conf
-echo "IMAGE_INSTALL_append_zynqmp += \" kernel-module-zynqmp-r5-remoteproc\"" >>$CONF
-echo "IMAGE_INSTALL_append = \" packagegroup-petalinux-openamp\"" >> $CONF
-echo "IMAGE_INSTALL_append+= \"openssh openssh-sshd openssh-sftp openssh-sftp-server\"" >> $CONF
+if [ ! -r $CONF.orig ]; then
+    # first run save what we are starting with
+    cp $CONF $CONF.orig
+else
+    # other reset to orig
+    cp $CONF.orig $CONF
+fi
+echo 'IMAGE_INSTALL:append:zynqmp += " kernel-module-zynqmp-r5-remoteproc"' >>$CONF
+echo 'IMAGE_INSTALL:append = " packagegroup-petalinux-openamp"' >> $CONF
+echo 'IMAGE_INSTALL:append = " openssh openssh-sshd openssh-sftp openssh-sftp-server"' >> $CONF
+echo 'EXTRA_IMAGE_FEATURES += " debug-tweaks"' >> $CONF
+
+# configure board specific rootfs tweaks
+echo "YAML_DT_BOARD_FLAGS:zcu102='{BOARD zcu102-rev1.0}'" >> $CONF
+echo "YAML_DT_BOARD_FLAGS:k26='{BOARD zynqmp-sm-k26-reva}'" >> $CONF
 
 echo "*** hack QEMU config"
 # Hack the QEMU config for user mode networking w/ TFTP
@@ -87,7 +100,18 @@ else
 fi
 echo "$QEMU_NET" >> $QEMU_CONF
 
+echo "*** hack k26 openamp.dtsi"
+# Hack the k26 openamp device tree source overlay
+K26_OA_DTSI=$XILINX_YOCTO_ROOT/sources/meta-som/recipes-bsp/device-tree/files/openamp.dtsi
+if [ ! -r $K26_OA_DTSI.orig ]; then
+    # first run save what we are starting with
+    cp $K26_OA_DTSI $K26_OA_DTSI.orig
+fi
+cp $PRJ_DIR/files/openamp_2022_1_fix.dtsi $K26_OA_DTSI
+
 # get PMU ROM
+# disabled, 2022.1 already does this
+if false; then
 if [ ! -r $DEPLOY_DIR/pmu-rom.elf ]; then
     echo "*** get PMU ROM"
     DEPLOY_DIR=$XILINX_YOCTO_ROOT/build/tmp/deploy/images/zcu102-zynqmp
@@ -96,8 +120,9 @@ if [ ! -r $DEPLOY_DIR/pmu-rom.elf ]; then
     tar -xvzf PMU_ROM.tar.gz
     cp -a  PMU_ROM/pmu-rom.elf $DEPLOY_DIR/pmu-rom.elf
 fi
+fi
 
-# modify DT
+# modify DT for zcu102
 echo "*** modify DT"
 DTS_ADD1=$PRJ_DIR/files/zcu102-rproc-adder.dtsi
 DTS_BASE_DIR=$XILINX_YOCTO_ROOT/build/workspace/sources/device-tree/device_tree
@@ -115,9 +140,6 @@ if [ ! -r $DTS_BASE.orig ]; then
 fi
 cat $DTS_BASE.orig $DTS_ADD1 >$DTS_BASE
 
-# mystery step
-echo "YAML_DT_BOARD_FLAGS='{BOARD zcu102-rev1.0}'" >> $CONF
-
 # force rebuild DTB
 echo "****** DT cleanall"
 MACHINE=zcu102-zynqmp bitbake -f -c cleanall device-tree
@@ -130,12 +152,23 @@ if [ -n "$FAKE_IT" ]; then
     exit 0
 fi
 
+# this step still seems to be needed in 2021.1
+# xilinx-bootbin uses bit file from deploy dir but
+# does not depend on bitstream-extraction.do_deploy
+#   MACHINE=zcu102-zynqmp bitbake -g openamp-image-minimal
+#   grep -Pe 'xilinx-bootbin.*-> "bitstream' task-depends.dot
+# gives only:
+#"xilinx-bootbin.do_prepare_recipe_sysroot" -> "bitstream-extraction.do_populate_sysroot"
+# Note: I lose this race ~50% of the time on a clean build but never from sstate
+# also since bitbake does gracefull shutdown, the "missing" bit file is normally
+# in the deploy dir after it stops as bitstream-extract.do_deploy has run in the
+# graceful shutdown phase.
 echo "*** build bit file to avoid race condition"
 MACHINE=zcu102-zynqmp bitbake virtual/bitstream
 
-echo "*** building image"
+echo "*** building image for zcu102"
 MACHINE=zcu102-zynqmp bitbake openamp-image-minimal
 
-# run it
-echo "*** running image"
-MACHINE=zcu102-zynqmp bitbake openamp-image-minimal
+# The kv260 BOOT.bin does not have any bit file so no race condition
+echo "*** building image for kv260"
+MACHINE=kv260-starter-kit bitbake openamp-image-minimal
